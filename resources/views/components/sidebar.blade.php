a5827532e7e49a8735e1be8b8acad652693772dd
Alexander <guguckins72@gmail.com>
2023-12-07 12:34:17 +0300

    new v1

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,18 +3,33 @@
         background: linear-gradient( to right, #D573DF, #CF842B);
         width: 469px;
         height: 100%;
+        display: grid;
+        grid-auto-columns: 1fr 1fr;
     }
-    .wraper__spisok{
-
+    .wraper__spisok ul{
+        display: flex;
+        flex-direction: column;
     }
-
+    .wraper__spisok li {
+        margin-bottom: 79px;
+    }
+    .wraper__spisok ul{
+        list-style: none;
+        padding: 16px;
+        margin: 0;
+    }
+    .wraper__spisok a{
+        text-decoration: none;
+        color: #FFFFFF;
+        font-size: 24px;
+    }
 </style>
     <div class="sidebar">
         <div class="wraper__spisok">
             <ul>
-                <li>Главная</li>
-                <li>Удалённые</li>
-                <li>Выполненные</li>
+                <li><a href="">Главная</a></li>
+                <li><a href="">Удалённые</a></li>
+                <li><a href="">Выполненные</a></li>
             </ul>
         </div>
         <div class="wraper__burgermenu__close">
