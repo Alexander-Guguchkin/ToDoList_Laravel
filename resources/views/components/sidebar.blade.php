22b7f790b4ee73304d515771c2fa31d78a4391a0
Alexander <guguckins72@gmail.com>
2023-12-07 12:09:42 +0300

    new v1

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,15 +1,26 @@
-<div class="sidebar">
-    <div class="wraper__spisok">
-        <ul>
-            <li>Главная</li>
-            <li>Удалённые</li>
-            <li>Выполненные</li>
-        </ul>
-    </div>
-    <div class="wraper__burgermenu__close">
-        <div class="burger-menu__close">
-            <span class="line3"></span>
-            <spam class="line4"></spam>
+<style>
+    .sidebar{
+        background: linear-gradient( to right, #D573DF, #CF842B);
+        width: 469px;
+        height: 100%;
+    }
+    .wraper__spisok{
+
+    }
+
+</style>
+    <div class="sidebar">
+        <div class="wraper__spisok">
+            <ul>
+                <li>Главная</li>
+                <li>Удалённые</li>
+                <li>Выполненные</li>
+            </ul>
+        </div>
+        <div class="wraper__burgermenu__close">
+            <div class="burger-menu__close">
+                <span class="line3"></span>
+                <spam class="line4"></spam>
+            </div>
         </div>
     </div>
-</div>
