8ea6b11a318524b41e4d7479aa0416924fdf36fc
Alexander <guguckins72@yandex.ru>
2023-12-15 08:40:46 +0300

    new v1

--- public/js/TodoList.js
+++ public/js/TodoList.js
@@ -2,40 +2,37 @@
 
 let input__Create__Task = document.querySelector('.input__create__task');
 let button__create = document.querySelector('.button__create');
-
-let button__delete = document.querySelector('.delete');
 let storage = [];
 
 button__create.addEventListener('click', ()=>{
     let value__input = input__Create__Task.value;
     storage.push(value__input);
     let output = document.querySelector('.output');
-    output.innerHTML += "             <div class=\"label__task create__task\">\n" +
-        "                <div class=\"text\">`${{value__input}}`</div>\n" +
-        "                <input type=\"checkbox\" class=\"checkbox-round\">\n" +
-        "                <div class=\"buttons\">\n" +
-        "                    <div class=\"edits\">\n" +
-        "                        <i class=\"ri-edit-line\"></i>\n" +
-        "                    </div>\n" +
-        "                    <button class=\"delete\">\n" +
-        "                        <i class=\"ri-delete-bin-2-line\"></i>\n" +
-        "                    </button>\n" +
-        "                </div>\n" +
-        "            </div>";
-    let text = document.querySelector('.text');
-    //
-    // text.textContent = value__input;
-
-    let button__edits = document.querySelectorAll('.edits');
-    for (const buttonEdit of button__edits) {
-        if (buttonEdit){
-            buttonEdit.addEventListener('click', ()=>{
-                console.log(1)
-            });
-        }
-    }
-
-
+    let labelTask = document.createElement('div');
+    labelTask.classList.add('label__task')
+    labelTask.classList.add('create__task');
+    output.append(labelTask);
+    let checkboxRound = document.createElement('input');
+    // Установка атрибутов для созданного элемента
+    checkboxRound.type = 'checkbox'; // Устанавливаем тип чекбокса
+    checkboxRound.id = 'myCheckbox'; // Устанавливаем уникальный идентификатор
+    checkboxRound.name = 'myCheckbox'; // Устанавливаем имя для отправки формы
+    labelTask.append(checkboxRound);
+    let buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    labelTask.append(buttons);
+    let  edits =document.createElement('div');
+    edits.classList.add('edits');
+    buttons.append(edits);
+    let iedit = document.createElement('i');
+    iedit.classList.add('ri-edit-line');
+    edits.append(iedit);
+    let deletes = document.createElement('div');
+    deletes.classList.add('delete');
+    buttons.append(deletes);
+    let idelete = document.createElement('i');
+    idelete.classList.add('ri-delete-bin-2-line');
+    deletes.append(idelete);
 });
 
 
